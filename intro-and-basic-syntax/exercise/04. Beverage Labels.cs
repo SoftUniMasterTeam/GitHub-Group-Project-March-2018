db2a31a276a5e6cb73f87ec43d5abb40796ec965
Borislavcho <bkirilov321@gamil.com>
2018-03-13 18:03:02 +0200

    Add problem 4 solution

---
+++ intro-and-basic-syntax/exercise/04. Beverage Labels.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04.Beverage_Labels
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            double volume = int.Parse(Console.ReadLine());
+            double energyContentPer100ML = int.Parse(Console.ReadLine());
+            double sugarContentPer100ML = int.Parse(Console.ReadLine());
+
+            double kcal = (volume / 100) * energyContentPer100ML;
+            double sugars = (volume / 100) * sugarContentPer100ML;
+
+            Console.WriteLine("{0}ml {1}:", volume, name);
+            Console.WriteLine("{0}kcal, {1}g sugars", kcal, sugars);
+        }
+    }
+}
