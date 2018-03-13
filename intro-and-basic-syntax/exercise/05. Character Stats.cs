de45fd65c21ceeb634d63c3c256bb5bc77395c76
Yagorundy <yagorundy@gmail.com>
2018-03-13 17:31:38 +0200

    Add problem 5 solution

---
+++ intro-and-basic-syntax/exercise/05. Character Stats.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var name = Console.ReadLine();
+            var curH = int.Parse(Console.ReadLine());
+            var maxH = int.Parse(Console.ReadLine());
+            var curE = int.Parse(Console.ReadLine());
+            var maxE = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Name: {name}");
+            Console.WriteLine("Health: |" + new string('|', curH) + new string('.', maxH - curH) + '|');
+            Console.WriteLine("Energy: |" + new string('|', curE) + new string('.', maxE - curE) + '|');
+        }
+    }
+}
