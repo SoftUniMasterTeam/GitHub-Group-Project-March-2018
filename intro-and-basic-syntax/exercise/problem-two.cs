52a67fbc4f288d8dacbb82b86a291bb607eba1f6
Yagorundy <yagorundy@gmail.com>
2018-03-13 15:56:22 +0200

    Added problem two solution

---
+++ intro-and-basic-syntax/exercise/problem-two.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = double.Parse(Console.ReadLine());
+            var b = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("{0:F2}", a * b);
+        }
+    }
+}
