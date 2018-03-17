d37125487db03aae6f0ea5a6012cde159f657f56
DKitov <D.kitov7@gmail.com>
2018-03-17 18:33:18 +0200

    Added comments in solution of Problem 1

--- intro-and-basic-syntax/exercise/01. Debit Card Number.cs
+++ intro-and-basic-syntax/exercise/01. Debit Card Number.cs
@@ -6,11 +6,13 @@ namespace _01._Debit_Card_Number
     {
         static void Main(string[] args)
         {
+            //* We receive from the Console 4 numbers
             int num1 = int.Parse(Console.ReadLine());
             int num2 = int.Parse(Console.ReadLine());
             int num3 = int.Parse(Console.ReadLine());
             int num4 = int.Parse(Console.ReadLine());
 
+            //* Printing the  debit card numbers in required format -> adding the needed additional digits
             Console.WriteLine($"{num1:d4} {num2:d4} {num3:d4} {num4:d4}");
         }
     }
