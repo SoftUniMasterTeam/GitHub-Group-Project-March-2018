d0aadf70a286cbfffe1572ad852d23ce6a0fe66c
AtanasKateliev <atanas_kateliev@abv.bg>
2018-03-18 01:11:03 +0200

    Problem 3.cs

---
+++ Problem 3.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Team_Problem_3
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double miles = double.Parse(Console.ReadLine());
+
+            double kilometers = miles * 1.60934;
+            Console.WriteLine($"{kilometers:f2}");
+        }
+    }
+}
