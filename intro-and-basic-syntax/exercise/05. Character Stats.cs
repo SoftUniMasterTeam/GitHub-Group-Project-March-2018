e459df1651f9a2ec3439338707a12d3abf29cfb5
BorislavKirilov <bobi4cveti@gmail.com>
2018-03-18 19:52:19 +0200

    Finished

--- intro-and-basic-syntax/exercise/05. Character Stats.cs
+++ intro-and-basic-syntax/exercise/05. Character Stats.cs
@@ -10,12 +10,15 @@ namespace _5
     {
         static void Main(string[] args)
         {
+            //Reading the input from the console 
             var name = Console.ReadLine();
             var ObichamTarator = int.Parse(Console.ReadLine()); // Changed currentHealth -> ObichamTarator
             var maxHealth = int.Parse(Console.ReadLine());
             var currrentEnergy = int.Parse(Console.ReadLine());
             var maxEnergy = int.Parse(Console.ReadLine());
 
+
+            //Printing the information
             Console.WriteLine($"Name: {name}");
             Console.WriteLine("Health: |" + new string('|', ObichamTarator) + new string('.', maxHealth - ObichamTarator) + '|');
             Console.WriteLine("Energy: |" + new string('|', currrentEnergy) + new string('.', maxEnergy - currrentEnergy) + '|');
