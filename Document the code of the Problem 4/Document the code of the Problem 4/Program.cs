ae6872d805a85ef22929525587e364fa1d0cf3b3
AtanasKateliev <atanas_kateliev@abv.bg>
2018-03-18 23:58:38 +0200

    Document the code-Problem 4

---
+++ Document the code of the Problem 4/Document the code of the Problem 4/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04.Beverage_Labels
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Reading the input from the console
+            string name = Console.ReadLine(); // name of product
+            double volume = int.Parse(Console.ReadLine()); //  volume of product
+            double energyContentPer100ML = int.Parse(Console.ReadLine()); //energy content per 100ML
+            double sugarContentPer100ML = int.Parse(Console.ReadLine());  //sugar content per 100ML
+
+            //Calculating the amount of kcal and sugar
+            double kcalAmount = (volume / 100) * energyContentPer100ML; //amount kall
+            double sugarsAmount = (volume / 100) * sugarContentPer100ML; //amount sugars
+
+            //Printing the information
+            Console.WriteLine("{0}ml {1}:", volume, name);
+            Console.WriteLine("{0}kcal, {1}g sugars", kcalAmount, sugarsAmount);
+        }
+    }
+}
