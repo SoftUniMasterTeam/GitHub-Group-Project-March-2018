1bf17a42b4904c4aa94ced8661828d6f77fd20b2
Yagorundy <yagorundy@gmail.com>
2018-03-18 18:00:54 +0200

    Comment problem 3

--- intro-and-basic-syntax/exercise/03. Miles To Kilometers.cs
+++ intro-and-basic-syntax/exercise/03. Miles To Kilometers.cs
@@ -1,15 +1,16 @@
 ﻿using System;
 
-namespace Problem MilesToKilometers
+namespace MilesToKilometers
 {
-    class Program
+    class Program   // Program that converts miles to kilometres
     {
         static void Main(string[] args)
         {
+            // Read from console
             double miles = double.Parse(Console.ReadLine());
 
-            double kilometers = miles * 1.60934;
-            Console.WriteLine($"{kilometers:f2}");
+            double kilometers = miles * 1.60934;    // convert miles to kilometres
+            Console.WriteLine($"{kilometers:f2}");  // print result
         }
     }
 }
